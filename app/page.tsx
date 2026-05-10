82e2b667b69b7a22b893a056cee96a55e59ec0ea
Doniyor <doniyoryusupob246@gmail.com>
2026-05-10 20:33:26 +0300

    added login and register page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
 import CategoryFilter from '@/components/shared/category-filter';
-import { Container } from '@/components/shared/container';
-import { Footer } from '@/components/shared/footer';
-import { Header } from '@/components/shared/header';
 import HeroSection from '@/components/shared/hero-section';
-import { Nav } from '@/components/shared/nav';
 import ProductGrid from '@/components/shared/product-grid';
 
 export default function Home() {
--- components/shared/login-form.tsx
+++ components/shared/login-form.tsx
@@ -1,6 +1,6 @@
 'use client';
 import React from 'react';
-import { Controller, useForm } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 import z from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { cn } from '@/lib/utils';
