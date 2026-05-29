b0b79ace9e66c7a23097e56ea59e267f0ba976a6
Doniyor <doniyoryusupob246@gmail.com>
2026-05-30 01:57:29 +0300

    add force dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import CategoryFilter from '@/components/shared/category-filter';
 import HeroSection from '@/components/shared/hero-section';
 import ProductGrid from '@/components/shared/product-grid';
-
+export const dynamic = 'force-dynamic';
 export default function Home() {
   return (
     <div>
